2abd406fb88fef01f85d7b31c8d0082a1a4d961f
Theringeye <ericmartinscastro@gmail.com>
2021-10-21 21:30:03 -0300

    Commit inicial

---
+++ src/main/java/br/com/ufg/sistred/SistredApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufg.sistred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ufg/sistred/SistredApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufg.sistred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
