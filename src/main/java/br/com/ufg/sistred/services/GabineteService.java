061447ad0faa062f4fe3ecf4b3d7a5ca9e74030e
Murilo Libaneo <mclibaneo@hotmail.com>
2022-02-23 11:17:27 -0300

    add gabinete resource e service

--- src/main/java/br/com/ufg/sistred/services/GabineteService.java
+++ src/main/java/br/com/ufg/sistred/services/GabineteService.java
@@ -1,31 +1,80 @@
 package br.com.ufg.sistred.services;
 
-import java.util.Optional;
-
+import java.util.ArrayList;
+import java.util.List;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import br.com.ufg.sistred.domain.Gabinete;
+import br.com.ufg.sistred.domain.HistoricoAtivo;
+import br.com.ufg.sistred.domain.Movimentacao;
+import br.com.ufg.sistred.dto.GabineteDTO;
+import br.com.ufg.sistred.dto.HistoricoAtivoDTO;
+import br.com.ufg.sistred.dto.MovimentacaoDTO;
 import br.com.ufg.sistred.repositories.GabineteRepository;
 
 @Service
-public class GabineteService {
+public class GabineteService {	
+	
+	@Autowired
+	private GabineteRepository entityRepository;
 
 	@Autowired
-	private GabineteRepository gabineteRepository;
+	private ModelMapper modelMapper;
 
-	public Gabinete findById(Integer id) {
+	public GabineteDTO salvar(GabineteDTO gabineteDTO) {
+		Gabinete gabinete = modelMapper.map(gabineteDTO, Gabinete.class);
+		return converteEntityToDTO(entityRepository.save(gabinete));
+	}
 
-		Optional<Gabinete> obj = gabineteRepository.findById(id);
-		return obj.orElse(null);
+	public GabineteDTO findById(Integer id) {
+		Gabinete gabinete = entityRepository.findById(id).get();
+		return converteEntityToDTO(gabinete);
+	}
+
+	public List<GabineteDTO> findAll() {
+		List<Gabinete> lista = entityRepository.findAll();
+
+		List<GabineteDTO> listaDTO = converteEntityToDTO(lista);
+
+		return listaDTO;
+	}
+
+	public void deletar(Integer id) {
+		Gabinete gabinete = entityRepository.findById(id).get();
+		entityRepository.delete(gabinete);
 
 	}
 
-	public Gabinete save(Gabinete gabinete) {
+	private GabineteDTO converteEntityToDTO(Gabinete gabinete) {
+		GabineteDTO gabineteDTO = new GabineteDTO();
+		gabineteDTO = modelMapper.map(gabinete, GabineteDTO.class);
+		return gabineteDTO;
+	}
 
-		gabineteRepository.save(gabinete);
-		return gabinete;
+	private List<GabineteDTO> converteEntityToDTO(List<Gabinete> lista) {
 
+		GabineteDTO gabineteDTO = new GabineteDTO();
+		ArrayList<GabineteDTO> listaDTO = new ArrayList<GabineteDTO>();
+
+		for (Gabinete gabinete : lista) {
+
+			gabineteDTO = modelMapper.map(gabinete, GabineteDTO.class);
+
+			for (HistoricoAtivo historicoAtivo : gabinete.getListaHistoricoAtivo()) {
+
+				gabineteDTO.getListaHistoricoAtivoDTO().add(modelMapper.map(historicoAtivo, HistoricoAtivoDTO.class));
+			}
+
+			for (Movimentacao movimentacao : gabinete.getListaMovimentacao()) {
+
+				gabineteDTO.getListaMovimentacaoDTO().add(modelMapper.map(movimentacao, MovimentacaoDTO.class));
+			}
+
+			listaDTO.add(gabineteDTO);
+		}
+
+		return listaDTO;
 	}
 
 }
