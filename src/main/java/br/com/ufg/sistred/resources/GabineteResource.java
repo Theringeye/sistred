effade5eaafda532e933b8b138b19a70ecf49f5b
theringeye <ericmartinscastro@gmail.com>
2022-02-23 16:18:39 -0300

    gabinete

--- src/main/java/br/com/ufg/sistred/resources/GabineteResource.java
+++ src/main/java/br/com/ufg/sistred/resources/GabineteResource.java
@@ -1,26 +1,61 @@
 package br.com.ufg.sistred.resources;
 
+import java.net.URI;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import br.com.ufg.sistred.domain.Gabinete;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import br.com.ufg.sistred.dto.GabineteDTO;
 import br.com.ufg.sistred.services.GabineteService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/gabinete")
 public class GabineteResource {
-
+	
 	@Autowired
 	private GabineteService gabineteService;
+	
+	@PostMapping
+	public ResponseEntity<GabineteDTO> salvar(@RequestBody GabineteDTO gabineteDTO) {
 
-	@PostMapping("/salvar")
-	public ResponseEntity<Gabinete> salvar(@RequestBody Gabinete gabinete) {
+		gabineteDTO = gabineteService.salvar(gabineteDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(gabineteDTO.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 
-		return ResponseEntity.ok(gabineteService.save(gabinete));
 	}
+
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<GabineteDTO> findById(@PathVariable Integer id) {
+
+		return ResponseEntity.ok().body(gabineteService.findById(id));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<GabineteDTO>> findAll() {
+
+		List<GabineteDTO> listaGabineteDTO = gabineteService.findAll();
+
+		return ResponseEntity.ok().body(listaGabineteDTO);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+
+		gabineteService.deletar(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	
 
 }
